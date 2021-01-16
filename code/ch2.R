5c0deac1cf02333a005e9c2f7b8d5dfad584fb1f
Ryan Beach <beachr90@gmail.com>
2021-01-16 14:35:03 -0500

    complete ch 2.3, pickup ch 2.4: making the model go

--- code/ch2.R
+++ code/ch2.R
@@ -5,3 +5,9 @@
 ways <- c(0, 3, 8, 9, 0) # ways to produce data
 ways/sum(ways) # plausibility
 
+## 2.3 - Components Of the Model
+
+dbinom(x = 6, size = 9, prob = 0.5) # binomial distribution;
+# likelihood of data: '6' water in '9' tosses (N), under any value of p.
+
+## 2.4 - Making the Model 'Go'
