4848ed2c58f1ba6002c0a0bb91472005b0480d58
Ryan Beach <beachr90@gmail.com>
2021-02-01 17:54:49 -0500

    complete linear prediction, pickup curves from lines (4.5)

--- code/ch4.R
+++ code/ch4.R
@@ -155,3 +155,183 @@ cov2cor(vcov(m4.1)) # correlation matrix
 post <- extract.samples(m4.1, n = 1e4)
 head(post)
 precis(post)
+
+## 4.37 - Linear prediction: plot height ~ weight -> how do they covary?
+plot(d2$height ~ d2$weight)
+
+## 4.38 - Simulate lines
+set.seed(2971) # reproduce text example
+N <- 100 # 100 lines
+a <- rnorm(N, 178, 20)
+b <- rnorm(N, 0, 10)
+
+# now (above, 4.38) we have 100 pairs of alpha and beta values
+
+## 4.39 - Plot lines
+plot(NULL, xlim=range(d2$weight), ylim = c(-100, 400),
+     xlab = "weight", ylab = "height")
+abline(h = 0, lty = 2)
+abline(h = 272, lty = 1, lwd = 0.5)
+mtext("b ~ dnorm(0,10)")
+xbar <- mean(d2$weight)
+for(i in 1:N) curve(a[i] + b[i]*(x - xbar),
+                    from = min(d2$weight), to = max(d2$weight), add = TRUE,
+                    col = col.alpha("black", 0.2))
+
+## 4.40 - simulate relationship and see what this means for beta (using log-normal)
+b <- rlnorm(1e4, 0, 1)
+dens(b, xlim = c(0,5), adj = 0.1)
+
+## 4.41 - prior predictive sim. using log-normal prior
+set.seed(2971) # reproduce text e.g.
+N <- 100 # 100 lines
+a <- rnorm(N, 178, 20)
+b <- rlnorm(N, 0, 1)
+
+plot(NULL, xlim=range(d2$weight), ylim = c(-100, 400),
+     xlab = "weight", ylab = "height")
+abline(h = 0, lty = 2)
+abline(h = 272, lty = 1, lwd = 0.5)
+mtext("b ~ dnorm(0,10)")
+xbar <- mean(d2$weight)
+for(i in 1:N) curve(a[i] + b[i]*(x - xbar),
+                    from = min(d2$weight), to = max(d2$weight), add = TRUE,
+                    col = col.alpha("black", 0.2))
+
+
+## 4.42 - Build the posterior approximation
+library(rethinking)
+data(Howell1)
+d <- Howell1
+d2 <- d[d$age >= 18, ]
+
+# define the average weight, x-bar
+xbar <- mean(d2$weight)
+
+# fit model
+m4.3 <- quap(
+  alist(
+    height ~ dnorm(mu, sigma),
+    mu <- a + b*(weight - xbar),
+    a ~ dnorm(178, 20),
+    b ~ dlnorm(0, 1),
+    sigma ~ dunif(0, 50)
+  ),
+  data = d2)
+
+## 4.44 - tables of marginal posterior distributions of params.
+precis(m4.3)
+
+## 4.45 - variance-covariance matrix
+round(vcov(m4.3), 3)
+pairs(m4.3) # marginal posterios and covariance
+
+## 4.46 - plotting posterior inference against the data
+plot(height ~ weight, data = d2, col=rangi2)
+post <- extract.samples(m4.3)
+a_map <- mean(post$a)
+b_map <- mean(post$b)
+curve(a_map + b_map*(x-xbar), add = TRUE)
+
+## 4.47 - visualizing uncertainty in the regression relationship
+post <- extract.samples(m4.3)
+post[1:5, ] # each row is a correlated random sample
+# paired values of alpha and beta define a line
+
+## 4.48 - add more data to see the change in scatter of lines (re-estimate model)
+N <- 10
+dN <- d2[1:N, ] # extract first 10 cases
+mN <- quap(
+  alist(
+    height ~ dnorm(mu, sigma),
+    mu <- a + b*(weight - mean(weight)),
+    a ~ dnorm(178, 20),
+    b ~ dlnorm(0, 1),
+    sigma ~ dunif(0, 50)
+  ), data = dN)
+
+## 4.49 - plot 20 of these lines to view uncertainty
+# extract 20 samples from the posterior
+post <- extract.samples(mN, n = 20)
+
+# display raw data and sample size
+plot(dN$weight, dN$height,
+     xlim = range(d2$weight), ylim = range(d2$height),
+     col = rangi2, xlab = "weight", ylab = "height")
+mtext(concat("N = ", N))
+
+# plot the lines with transparency
+for(i in 1:20)
+  curve(post$a[i] + post$b[i]*(x - mean(dN$weight)),
+        col = col.alpha("black", 0.3), add = TRUE)
+
+## 4.50 - compute arbitrary interval(s) using cloud of regression lines 
+# E.g. 50 kilograms ($weight)
+
+post <- extract.samples(m4.3)
+mu_at_50 <- post$a + post$b * (50 - xbar)
+dens(mu_at_50, col = rangi2, lwd = 2, xlab = "mu|weight = 50")
+
+
+## 4.51 - 89% compatibility interval of mu at 50 kg
+PI(mu_at_50, prob = 0.89)
+
+## 4.53 - repeat above for every weight value (not just 50 kg)
+mu <- link(m4.3)
+str(mu) # matrix of values of mu (each row = sample from post. dist.)
+
+## 4.54 - distribution of mu for each weight(i)
+# define sequence of weights to computer predictions for
+# these values will be on the horizontal axis
+weight.seq <- seq(from = 25, to = 70, by = 1)
+
+# use link to compute mu
+# for each sample from posterior
+# and for each  weight.seq
+mu <- link(m4.3, data = data.frame(weight = weight.seq))
+str(mu)
+
+## 4.55 - plot distribution of mu values at each height 
+# use type = 'n' to hide raw data
+plot(height ~ weight, d2, type = 'n')
+
+# loop over samples and plot each mu value
+for(i in 1:100)
+  points(weight.seq, mu[i, ], pch = 16, col = col.alpha(rangi2, 0.1))
+
+## 4.56 - summarise the distribution for each weight value
+mu.mean <- apply(mu, 2, mean) # avg. mu at each weight value
+mu.PI <- apply(mu, 2, PI, prob=0.89) # lower/upper bounds for each weight value
+
+## 4.57 - plot summaries on top of the data
+# plot raw data
+# fading out points to make line and interval more visible
+plot(height ~ weight, data = d2, col = col.alpha(rangi2, 0.5))
+
+# plot the MAP line, aka the mean mu for each weight
+lines(weight.seq, mu.mean)
+
+# plot a shaded region for 89% PI
+shade(mu.PI, weight.seq)
+
+## 4.59 - Simulate heights (predicting sigma)
+sim.height <- sim(m4.3, data = list(weight = weight.seq))
+str(sim.height) # simulated heights
+
+## 4.60 - summarize simulated heights
+height.PI <- apply(sim.height, 2, PI, prob = 0.89) # 89% posterior prediction interval of observable heights
+# across values of weight in weight.seq
+
+## 4.61 - Plot!
+# plot raw data
+plot(height ~ weight, d2, col = col.alpha(rangi2, 0.5))
+
+# draw MAP line
+lines(weight.seq, mu.mean)
+
+# draw HPDI region for line
+shade(mu.HPDI, weight.seq)
+
+# draw PI region for simulated heights
+shade(height.PI, weight.seq)
+
