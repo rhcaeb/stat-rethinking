1ccd4c3eedb476e7dba093de8e3e229ac25f7537
Ryan Beach <beachr90@gmail.com>
2021-03-19 14:51:47 -0400

    finish ch 6, pick up ch 7

--- code/ch6.R
+++ code/ch6.R
@@ -220,3 +220,42 @@ m6.10 <- quap(
   ), data = d2)
 precis(m6.10)
 
+## 6.25 
+
+## simple simulation -> invent some strength of association
+N <- 200 # number of grandparent-parent-child triads
+b_GP <- 1 # direct effect of G on P
+b_GC <- 0 # direct effect of G on C
+b_PC <- 1 # direct effect of P on C
+b_U <- 2 # direct effect of U on P and C
+
+## use the above 'slopes' to draw random observations
+set.seed(1)
+U <- 2*rbern(N, 0.5) - 1
+G <- rnorm(N)
+P <- rnorm(N, b_GP*G + b_U*U)
+C <- rnorm(N, b_PC*P + b_GC*G + b_U*U)
+d <- data.frame(C = C, P = P, G = G, U = U)
+
+## simple regression model of C on P and G
+m6.11 <- quap(
+  alist(
+    C ~ dnorm(mu, sigma),
+    mu <- a + b_PC*P + b_GC*G,
+    a ~ dnorm(0, 1),
+    c(b_PC, b_GC) ~ dnorm(0, 1),
+    sigma ~ dexp(1)
+  ), data = d)
+precis(m6.11)
+
+## need to measure 'U' for collider bias; regress on U
+m6.12 <- quap(
+  alist(
+    C ~ dnorm(mu, sigma),
+    mu <- a + b_PC*P + b_GC*G + b_U*U,
+    a ~ dnorm(0,1),
+    c(b_PC, b_GC, b_U) ~ dnorm(0,1),
+    sigma ~ dexp(1)
+  ), data = d)
+precis(m6.12)
+
