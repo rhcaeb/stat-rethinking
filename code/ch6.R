f1e05cc3d252b03bbe9d88f65ca626828aa9ec57
Ryan Beach <beachr90@gmail.com>
2021-03-19 12:01:42 -0400

    update remote pickup 6.3.2 haunted DAGs!

--- code/ch6.R
+++ code/ch6.R
@@ -184,4 +184,39 @@ fungus <- rbinom(N, size = 1, prob = 0.5 - treatment*0.4 + 0.4*M)
 h1 <- h0 + rnorm(N, 5 + 3*M)
 d2 <- data.frame(h0 = h0, h1 = h1, treatment = treatment, fungus = fungus)
 
+##### Collidar bias ----
+
+library(rethinking)
+d <- sim_happiness(seed = 1977, N_years = 1000)
+precis(d)
+
+## 6.22
+
+d2 <- d[d$age > 17, ] # only adults
+d2$A <- (d2$age - 18) / (65-18)
+
+## 6.23 
+
+d2$mid <- d2$married + 1
+m6.9 <- quap(
+  alist(
+    happiness ~ dnorm(mu, sigma),
+       
+    a[mid] ~ dnorm(0, 1),
+    bA ~ dnorm(0, 2),
+    sigma ~ dexp(1)
+  ), data = d2)
+precis(m6.9, depth = 2)
+
+# compare the above model with one that omits marriage status
+
+m6.10 <- quap(
+  alist(
+    happiness ~ dnorm(mu, sigma),
+    mu <- a + bA * A,
+    a ~ dnorm(0, 1),
+    bA ~ dnorm(0, 2),
+    sigma ~ dexp(1)
+  ), data = d2)
+precis(m6.10)
 
